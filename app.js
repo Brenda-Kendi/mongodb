f8582fb5ee51763c2e5fb149a998f4ee961bc185
Brenda-Kendi <brendakendi15@gmail.com>
2020-12-09 17:16:40 +0300

    entry

---
+++ app.js
@@ -0,0 +1,67 @@
+require('dotenv').config()
+
+var createError = require('http-errors');
+var express = require('express');
+var app = express();
+const mongoose = require ('mongoose')
+const bodyParser = require ('body-parser')
+
+
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+const blogRouter = require('./routes/blog')
+const Blog = require ('./models/Blog')
+
+
+
+
+
+//connecting to database
+mongoose
+.connect(process.env.MONGO_URL, { useNewUrlParser: true ,useUnifiedTopology: true , useCreateIndex:true})
+.then(()=>console.log('----database connected successfully----'))
+.catch(err=>{console.log(err)})
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/blogs',blogRouter);
+
+//defining main route
+app.get('/',async (req,res)=>{
+ await Blog.find({},(err,blogs)=>{
+    if(!err){
+      res.render('index', {blogs : blogs})
+    }
+    else{
+      console.log(err)
+    }
+  })
+})
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
